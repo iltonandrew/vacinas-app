323e5906b9c4853f6d699364a287efb1bc704bf2
Ilton Andrew <ilton.andrew@pipefy.com>
2021-06-23 01:46:36 -0300

    🍱 Add assets

---
+++ assets/colors.ts
@@ -0,0 +1,5 @@
+export const darkBase = '#34252f';
+export const darkBlue = '#19647e';
+export const lightBlue = '#98D2EB';
+export const lightGreen = '#c4ffb2';
+export const darkGreen = '#b7e3cc';
