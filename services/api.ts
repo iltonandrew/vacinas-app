96d38b3b6f8e117b74be9d21d319aa77ba3d90b2
Ilton Andrew <ilton.andrew@pipefy.com>
2021-07-04 18:40:42 -0300

    👽️ Create getUsers request

--- services/api.ts
+++ services/api.ts
@@ -3,3 +3,8 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://vacinas-app-mhanprpxzq-uc.a.run.app',
 });
+
+export const getUsers = api
+  .get('users')
+  .then((response) => console.log(response.data))
+  .catch((error) => console.error(error));
